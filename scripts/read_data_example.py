c0455cdf3d3152e0ae2a2333c7f913ef6918eb09
Linnea Persson <laperss@kth.se>
2017-04-29 11:28:09 +0200

    loop over all data and print

--- scripts/read_data_example.py
+++ scripts/read_data_example.py
@@ -6,6 +6,7 @@ import numpy as np
 import pprint
 
 file_name = '../assets/example_data/MYD05_L2.A2017109.0000.006.NRT.hdf'
+
 file = SD(file_name, SDC.READ)
 
 print('Data size: ')
@@ -14,35 +15,21 @@ print( file.info() )
 
 datasets = file.datasets()
 
+
+def scaled_data(sds_obj):
+    data = sds_obj.get()
+    offset = sds_obj.attributes()['add_offset']
+    factor = sds_obj.attributes()['scale_factor']
+    scaled_data = (data - offset)*factor
+    return scaled_data
+
+
 # Print data contents
 print('\nData content:')
-for idx,sds in enumerate(datasets.keys()):
-    print( idx,sds )
+for key in datasets.keys():
+    print('\n' + key)
+    sds_obj = file.select(key)
+    data = scaled_data(sds_obj)
+    print(data)
 
 
-sds_obj = file.select('Solar_Azimuth') # select sds
-
-data = sds_obj.get() # get sds data
-
-#----------------------------------------------------------------------------------------#
-# get attributes
-
-print('\nData attributes:')
-pprint.pprint( sds_obj.attributes() )
-
-for key, value in sds_obj.attributes().items():
-    print( key, value )
-    if key == 'add_offset':
-        add_offset = value  
-    if key == 'scale_factor':
-        scale_factor = value
-
-print('\nScale factors:')        
-print( 'add_offset', add_offset, type(add_offset) )
-print( 'scale_factor', scale_factor, type(scale_factor) )
-
-data = (data - add_offset) * scale_factor
-
-
-print('\nScaled data: :')
-print( data )
