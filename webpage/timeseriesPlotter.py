031718f5d862911646fb2f1e0d93af0e0802a56d
Emil Wärnberg <emilwa@kth.se>
2017-04-30 12:51:22 +0200

    New version of webpage.

--- webpage/timeseriesPlotter.py
+++ webpage/timeseriesPlotter.py
@@ -4,6 +4,7 @@ import matplotlib
 matplotlib.use('AGG')
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import matplotlib.pyplot as plt
+import scipy.stats
 
 # For Flask output
 from io import BytesIO
@@ -13,6 +14,8 @@ class TimeSeriesPlotter(object):
         self.df = pd.read_pickle(datatable_path)
         # Remove erroreneous data
         self.df = self.df[self.df.cloud>-5]
+
+        self.df[['aod','cloud', 'vapor']] = self.df[['aod','cloud', 'vapor']].apply(scipy.stats.zscore)
 
         # Prepare an image for when someone clicks on data that isn't available
         with open("../assets/no_data.png", "rb") as f:
@@ -24,13 +27,13 @@ class TimeSeriesPlotter(object):
         onePoint = self.df[np.logical_and(self.df.long==lon, self.df.lat==lat)]
 
         if onePoint.size > 0:
-            plt.figure(figsize=(6.4, 4.8), dpi=100)
-            plt.plot(onePoint.day, onePoint.aod, label="Aerosols")
+            plt.figure(figsize=(6.4, 6.4), dpi=100)
+            plt.plot(onePoint.day, onePoint.aod, label="Aerosols optical depth")
             plt.plot(onePoint.day, onePoint.cloud, label="Cloudiness")
             plt.plot(onePoint.day, onePoint.vapor, label="Atmospheric vapor")
-            plt.title(("Values at ({},{})".format(lon,lat)))
+            #plt.title(("Values at ({},{})".format(lon,lat)))
             plt.xlabel("Day in year 2017")
-            plt.ylabel("Arbitrary and different units")
+            plt.ylabel("Z-scored values")
             plt.legend()
             plt.tight_layout()
             png_output = BytesIO()
